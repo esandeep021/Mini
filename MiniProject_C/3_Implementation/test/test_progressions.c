e5bb3b926c5047b73e6ddbfeea0776a0b4934a1b
stepin85209 <35224798+stepin85209@users.noreply.github.com>
2020-10-07 09:17:06 +0530

    Update and rename test_calculator_operations.c to test_progressions.c

--- MiniProject_C/3_Implementation/test/test_progressions.c
+++ MiniProject_C/3_Implementation/test/test_progressions.c
@@ -2,14 +2,12 @@
 #include <CUnit/CUnit.h>
 
 /* Modify these two lines according to the project */
-#include <calculator_operations.h>
-#define PROJECT_NAME    "Calculator"
+#include "progressions.h"
+#define PROJECT_NAME    "Progressions"
 
 /* Prototypes for all the test functions */
-void test_add(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
+void test_ar_prog(void);
+void test_geo_prog(void);
 
 /* Start of the application test */
 int main() {
@@ -22,10 +20,8 @@ int main() {
   
   
   /* Add your test functions in this format for testing*/
-  CU_add_test(suite, "add", test_add);
-  CU_add_test(suite, "subtract", test_subtract);
-  CU_add_test(suite, "multiply", test_multiply);
-  CU_add_test(suite, "divide", test_divide);
+  CU_ap_test(suite, "ar_prog", test_ar_prog)
+  CU_add_test(suite, "geo_prog", test_geo_prog);
 
 
 /* Note: Do not edit START*/
@@ -42,31 +38,18 @@ int main() {
 }
 
 /* Write all the test functions */ 
-void test_add(void) {
-  CU_ASSERT(30 == add(10, 20));
+void test_ar_prog(void) {
+  CU_ASSERT(5 == ar_prog(1, 1, 5));
   
   /* Dummy fail*/
-  CU_ASSERT(1500 == add(750, 7500));
+  CU_ASSERT(10 == ar_prog(1, 1, 5));
 }
 
-void test_subtract(void) {
-  CU_ASSERT(-3 == subtract(0, 3));
+void test_gro_prog(void) {
+  CU_ASSERT(16 == geo_prog(1, 2, 5));
   
   /* Dummy fail*/
-  CU_ASSERT(1 == subtract(1000, 900));
+  CU_ASSERT(20 == geo_prog(2, 3, 3));
 }
 
-void test_multiply(void) {
-  CU_ASSERT(0 == multiply(1, 0));
-  
-  /* Dummy fail*/
-  CU_ASSERT(2 == multiply(2, 5));
-}
-
-void test_divide(void) {
-  CU_ASSERT(0 == divide(1, 0));
-  
-  /* Dummy fail*/
-  CU_ASSERT(3 == divide(2, 2));
-}
 
