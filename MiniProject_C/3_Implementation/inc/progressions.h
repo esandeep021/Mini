458b6c2a52b70ab601c7261510e1489af85a9c2d
stepin85209 <35224798+stepin85209@users.noreply.github.com>
2020-10-07 10:34:22 +0530

    Update and rename calculator_operations.h to progressions.h

---
+++ MiniProject_C/3_Implementation/inc/progressions.h
@@ -0,0 +1,19 @@
+/** 
+* @file progressions.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __PROGRESSIONS_H__
+#define __PROGRESSIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+
+int ar_prog(int a, int d, int n);
+
+
+int geo_prog(int a, int r, int n);
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
