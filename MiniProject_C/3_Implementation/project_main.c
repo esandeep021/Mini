2d260c7db6a05bc1a3d603d7c637b75da27171e4
stepin85209 <35224798+stepin85209@users.noreply.github.com>
2020-10-07 10:15:23 +0530

    Update and rename progressions.c to project_main.c

--- MiniProject_C/3_Implementation/project_main.c
+++ MiniProject_C/3_Implementation/project_main.c
@@ -1,24 +1,6 @@
 #include<stdio.h>
+#include<progressions.c>
 
-int ar_prog(int a,int d,int n){
-	int loop_var=1,next_term=a;
-	printf("%d",a);
-	for(;loop_var<n;loop_var++){
-		next_term+=d;
-		printf(" %d",next_term);
-	}
-	return next_term;
-}
-
-int geo_prog(int a,int r,int n){
-	int loop_var=1,next_term=a;
-	printf("%d",a);
-	for(;loop_var<n;loop_var++){
-		next_term*=n;
-		printf(" %d",next_term);
-	}
-	return next_term;
-}
 
 void main(){
 	int a,d,r,n,menu,x;
